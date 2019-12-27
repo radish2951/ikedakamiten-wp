3e75337f94dab6baa2cb6e074340c8e4dbb5ffab
Daiki Ikeda <tetra.chan.da.z.1000@gmail.com>
2019-12-27 22:07:36 +0900

    Add some layout changes

--- home.php
+++ home.php
@@ -15,34 +15,9 @@
         <p class="float"><?php echo get_option_with_newline('main_message'); ?></p>
     </section>
 
-    <!--section id="chochin">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin01-1.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin02-1.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin03-1.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin04.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin05.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin06.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin01-1.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin02-1.png">
-        <h2 class="float">世界に一つだけの提灯。</h2>
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin03-1.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin04.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin05.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin06.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin01-1.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin02-1.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin03-1.png">
-        <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/07/chochin04.png">
-    </section-->
-    <section id="try-chochin" class="product">
+    <section id="chochin" class="product">
     <h2 class="float"><?php echo get_option('chochin_slogan'); ?></h2>
-        <!--p class="button"><a href="">詳細</a></p-->
         <img class="float" src="/wp-content/uploads/2019/08/20150720_104052-Edit.jpg">
-        <!--
-        <img class="half float" src="/wp-content/uploads/2019/08/20141130_152833.jpg">
-        <img class="half float" src="/wp-content/uploads/2019/08/52475650_2228233893886068_3473433270245392384_n_.jpg">
-        <img class="half float" src="/wp-content/uploads/2019/08/50624138_2183548441687947_6510217595126284288_n_.jpg">
-        -->
         <p class="float"><?php echo get_option_with_newline('chochin_message'); ?></p>
         <div class="img-box">
             <img class="float" src="/wp-content/uploads/2019/08/20150627_164200-Edit.jpg">
@@ -52,12 +27,6 @@
     </section>
 
     <section id="fusuma" class="product">
-        <!--
-        <div id="fusuma-animation">
-            <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/08/fusuma_l.jpg">
-            <img src="http://ikedakamiten.0am.jp/wp-content/uploads/2019/08/fusuma_r.jpg">
-        </div>
-        -->
         <h2 class="float"><?php echo get_option('fusuma_slogan'); ?></h2>
         <img class="float" src="/wp-content/uploads/2019/08/IMG_1962.jpg">
         <p class="float"><?php echo get_option_with_newline('fusuma_message'); ?></p>
@@ -70,6 +39,7 @@
 
 <section id="news">
 <h2 class="float title">お知らせ</h2>
+<div class="articles">
 <?php
 query_posts( 'cat=3' );
 if ( have_posts()) {
@@ -80,13 +50,13 @@ if ( have_posts()) {
 <?php
         the_title('<h3><a href="' . get_permalink() . '">', '</a></h3>');
         the_date('', '<time>', '</time>');
-        the_content();
 ?>
 </article>
 <?php
     }
 }
 ?>
+</div>
     </section>
 </main>
 
