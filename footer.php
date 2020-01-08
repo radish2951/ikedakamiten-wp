ba0931837dede4d2870976c6e116ac25894556b3
Daiki Ikeda <tetra.chan.da.z.1000@gmail.com>
2020-01-08 21:22:44 +0900

    Remove unnecessary files

--- footer.php
+++ footer.php
@@ -1,18 +1,4 @@
 <footer id="footer">
-    <?php /*wp_nav_menu(array(
-        'container'  => false,
-        'items_wrap' => '<nav><ul>%3$s</ul></nav>',
-    ));*/ ?>
-<!--
-    <section id="footer-company-information">
-    <p><?php echo get_option('company_name'); ?></p>
-    <p>〒<?php echo get_option('postal_code'); ?></p>
-    <p><?php echo get_option('address'); ?></p>
-    <p>TEL: <?php echo get_option('tel'); ?></p>
-    <p>FAX: <?php echo get_option('fax'); ?></p>
-    <p>EMAIL: <?php echo get_option('email'); ?></p>
-    </section>
--->
     <p><?php echo get_option('company_name_en'); ?> All rights reserved.</p>
 </footer>
 <script src="<?php echo get_template_directory_uri(); ?>/float.js?v=<?php echo rand(); ?>"></script>
